23ccebff29c715ee29d15dccb950397a46758d91
hzuppur <h.zuppur@gmail.com>
2022-03-21 00:37:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Lunch-bot alpha 0.1v
+## Bot for creating text for choosing lunch locations
+
+This text can be posted to chat and then people can react
+to the text to indicate their preferred place to go for lunch.
+
+Example output of the script is down below.
+
+----
+## Päevapakkumised 21 March 
+### 1️⃣ Delta kohvik:
+* Pardi-confit kartulite/riisi/tatra, kastme ja salatiga. 5,80€
+Duck confit with potates/rice/buckwheat, sauce and salad. 5,80€
+* Rebitud sealiha kartulite/tatra, kastme ja salatiga. 5,50€
+Pulled pork with potatoes/buckwheat, sauce and salad. 5,50€
+* Kapsakotlet kartulite/riisi/tatra, kastme ja salatiga (V). 4,90€
+Cabbage cutlet with potatoes/rice/buckwheat, sauce and salad (V). 4,90€
+* Kana poolkoib kartulite/tatra, kastme ja salatiga. 4,80€
+Chicken drumsticks with potatoes/buckwheat, sauce and salad. 4,80€
+* Köögivilja püreesupp. 3,00/2,00€
+Vegetable puree soup. 3,00/2,00€
+### 2️⃣ Cafe Naiiv:
+* Krõbekana kauss. 4,90€
+* Falafeli kauss. 4,50€
+### 3️⃣ The Grill:
+* Gruusia Odzahuri: praetud sealiha kartulitega, paprika ja sibulaga, kaste, leib, maitsevesi. 5,00€
+
+----
+
+## Dependency's
+requests - for getting the päevapakkumised.ee page
+
+bs4 - for parsing the HTML
+
+pyperclip - for copying the text to clipboard
---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+import pyperclip as pc
+
+EMOJI_NUMBERS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+OFFERS_URL = "https://xn--pevapakkumised-5hb.ee/tartu"
+
+def get_offers():
+    html_doc = requests.get(OFFERS_URL).content
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    meal_locations = soup.find_all(lambda tag: tag.name == "div" and tag.get("class") == ["offerLayout"])
+    location_offers = {}
+
+    for location in meal_locations:
+        name = location.find('h3').text.strip()
+        offers = [i.text.strip() for i in
+                  location.find_all(lambda tag: tag.name == "div" and tag.get("class") == ["offer"])]
+        location_offers[name] = offers
+
+    return location_offers
+
+
+def get_offers_string(selected_locations):
+    today = date.today()
+    offers_string = f"## Päevapakkumised {today.strftime('%d %B')} \n"
+    location_offers = get_offers()
+
+    for i in range(len(selected_locations)):
+        selected_location = selected_locations[i]
+        offers_string += f"### {EMOJI_NUMBERS[i]} {selected_location}:\n"
+        for offer in location_offers[selected_location]:
+            offers_string += f"* {offer}\n"
+
+    return offers_string
+
+
+if __name__ == '__main__':
+    meal_offers = get_offers_string(["Delta kohvik", "Cafe Naiiv", "The Grill"])
+    pc.copy(meal_offers)
+    print(meal_offers)
